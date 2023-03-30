9ebb832d50b316c320c455db8e4fa08e9c4dea2d
Abhishek Kumar Sah <awvisek99@gmail.com>
2023-03-30 20:48:50 +0545

    commit
    Test 3 commit to dev

---
+++ MEVN/Test_3/Q_1 & 3/test_3/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <Header/>
+   <h1>Hello user</h1>
+  
+</template>
+
+<script>
+import Header from './Header.vue';
+export default {
+   name:'Home',
+   components:{
+       Header
+   },
+   mounted() {
+           let user= localStorage.getItem('user-info');
+           if(!user) {
+               this.$router.push({name:'SignUp'})
+           }
+       },
+}
+</script>
---
+++ MEVN/Test_3/Q_2/app.js
@@ -0,0 +1,7 @@
+const math = require("./math");
+
+console.log(math.add(5, 10)); // Output: 15
+console.log(math.subtract(20, 5)); // Output: 15
+console.log(math.multiply(3, 4)); // Output: 12
+console.log(math.divide(20, 5)); // Output: 4
+console.log(math.divide(20, 0)); // Throws an error: Cannot divide by zero
